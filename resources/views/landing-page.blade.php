531552f07b59153bf4dc18782e60a64721fc81a5
Helios-Sinatrya <gilangminecraft21@gmail.com>
2021-11-12 18:01:58 +0700

    adding css

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="public/css/style.css">
     <title>Traveroy</title>
   </head>
   <body>
@@ -31,7 +31,7 @@
                         <a class="nav-link" href="#">History</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <button class="btn btn-transparent dropdown-toggle d-flex align-items-center" id="LangSelect" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button class="btn btn-transparent dropdown-toggle d-flex align-items-center lang-select" id="LangSelect" data-bs-toggle="dropdown" aria-expanded="false">
                             <span style="font-size: 1.5em;" class="iconify" data-icon="codicon:globe"></span>
                         </button>
                     </li>
@@ -40,10 +40,11 @@
         </div>
     </nav>
     <main class="container">
-        <div class="row">
-            <div class="col">         
+        <div class="row landing-face">
+            <div class="col-12 d-flex justify-content-center align-items-center">         
                 <div class="landing-text">
                     <h1>Traveroy</h1>
+                    <p>Make Your Journey Fun</p>
                 </div>
             </div>
         </div>
