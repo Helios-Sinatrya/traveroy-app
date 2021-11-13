356ac87d38850987b0c6fe445fbc5e8f8c581cbb
Helios-Sinatrya <gilangminecraft21@gmail.com>
2021-11-14 00:58:49 +0700

    -adding bacgkround <br> -adding font <br> -fixing navbar position

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -6,45 +6,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&family=Righteous&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="/css/style.css">
     <title>Traveroy</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-        <div class="container">
-            <a class="navbar-brand" href="#">TRAVEROY</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Facility</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Gallery</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">History</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <button class="btn btn-transparent dropdown-toggle d-flex align-items-center lang-select" id="LangSelect" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span style="font-size: 1.5em;" class="iconify" data-icon="codicon:globe"></span>
-                        </button>
-                    </li>
-                </ul>
+    <main class="relative">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <a class="navbar-brand font-right" href="#">Traveroy</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center font-philo font-bold">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Facility</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">History</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <button class="btn dropdown-toggle d-flex align-items-center lang-select" id="LangSelect" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span style="font-size: 1.5em;" class="iconify" data-icon="codicon:globe"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li class="dropdown-item">
+                                    <a href="#">English</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a href="">Bahasa Indonesia</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-    <main class="container">
-        <div class="row landing-face">
-            <div class="col-12 d-flex justify-content-center align-items-center">         
-                <div class="landing-text">
-                    <h1>Traveroy</h1>
-                    <p>Make Your Journey Fun</p>
+        </nav>
+        <div class="container-fluid landing-face d-flex justify-content-center align-items-center text-white">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="landing-text text-center">
+                        <h1 class="font-right display-3">Traveroy</h1>
+                        <p class="font-philo">Make your journey fun</p>
+                    </div>
                 </div>
             </div>
         </div>
